16dbf2da766f1a25c30a389133fd658ad43e5050
takaheraw <takaheraw@gmail.com>
2018-09-16 16:00:19 +0900

    userのscope追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
+  ACTIVE_DURATION = ENV.fetch('USER_ACTIVE_DAYS', 7).to_i.days
+
   enum role: { admin: 1 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :oauth_access_tokens, class_name: 'Doorkeeper::AccessToken', foreign_key: :resource_owner_id
+
+  scope :recent, -> { order(id: :desc) }
+  scope :inactive, -> { where(arel_table[:current_sign_in_at].lt(ACTIVE_DURATION.ago)) }
+  scope :active, -> { confirmed.where(arel_table[:current_sign_in_at].gteq(ACTIVE_DURATION.ago)).joins(:account).where(accounts: { suspended: false }) }
+  scope :matches_email, ->(value) { where(arel_table[:email].matches("#{value}%")) }
+  scope :with_recent_ip_address, ->(value) { where(arel_table[:current_sign_in_ip].eq(value).or(arel_table[:last_sign_in_ip].eq(value))) }
 
   def activate_access_token(request)
     superapp = Doorkeeper::Application.find_by(superapp: true)
