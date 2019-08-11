9e2f191919eaf9594303b451490a782e78689e96
takaheraw <takaheraw@gmail.com>
2019-08-11 19:43:52 +0900

    simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,6 @@
 
 if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
-  if ENV['CIRCLE_ARTIFACTS']
-    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-    SimpleCov.coverage_dir(dir)
-  end
   SimpleCov.start 'rails' do
     add_group 'Services', 'app/services'
     add_group 'Presenters', 'app/presenters'
