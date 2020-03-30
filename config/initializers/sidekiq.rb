825bc8903d9d289f580b086b2b211360b37a6236
takaheraw <takaheraw@gmail.com>
2020-03-30 17:01:45 +0900

    update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,4 +11,4 @@ Sidekiq.configure_client do |config|
   config.redis = redis_params
 end
 
-#Sidekiq::Logging.logger.level = ::Logger.const_get('info'.upcase.to_s)
+Sidekiq.logger.level = ::Logger.const_get('info'.upcase.to_s)
